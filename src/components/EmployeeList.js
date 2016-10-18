3754fc4111de3581962d2256df97e50ac4552c04
Rex Low <qiweilow950823@gmail.com>
2016-10-18 21:11:51 +0800

    Added auth and main scene buckets

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+  render() {
+    return(
+      <View>
+        <Text>dasdasd</Text>
+        <Text>dasdasd</Text>
+        <Text>dasdasd</Text>
+        <Text>dasdasd</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
