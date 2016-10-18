63307486d9f38c6f3eb52093d089d4db4a0fb379
Rex Low <qiweilow950823@gmail.com>
2016-10-18 09:49:15 +0800

    Added redux thunk logic to user login authentication

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,7 +1,7 @@
 //one reducer responsibles for all thing authentication
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './../components/actions/types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './../components/actions/types';
 
-const INITIAL_STATE = { email: '', password: ''}
+const INITIAL_STATE = { email: '', password: '', user: null}
 
 export default (state = INITIAL_STATE, action) => {
   console.log(action);
@@ -10,6 +10,8 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, email: action.payload}; //make a new object, return a new state if there's a new value
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload};
+    case LOGIN_USER_SUCCESS:
+      return { ...state, user: action.payload};
     default:
       return state;
   }
