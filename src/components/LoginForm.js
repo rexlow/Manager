5f79bd9387754652206dcb5d0f85fc2fd0f2dfac
Rex Low <qiweilow950823@gmail.com>
2016-10-18 08:02:32 +0800

    Added reducer for passwordChanged

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 import { connect } from 'react-redux';
-import { emailChanged } from './actions/';
+import { emailChanged, passwordChanged } from './actions/';
 
 import Card from './common/Card';
 import CardSection from './common/CardSection';
@@ -12,6 +12,10 @@ import Button from './common/Button';
 class LoginForm extends Component {
   onEmailChange(text) {
     this.props.emailChanged(text);
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
   }
 
   render(){
@@ -31,6 +35,8 @@ class LoginForm extends Component {
             secureTextEntry={true}
             label="Password"
             placeholder="password"
+            onChangeText={this.onPasswordChange.bind(this)}
+            value={this.props.password}
           />
         </CardSection>
 
@@ -44,8 +50,9 @@ class LoginForm extends Component {
 
 const mapStateToProps = state => {
   return {
-    email: state.auth.email
+    email: state.auth.email,
+    password: state.auth.password
   }
 }
 
-export default connect(mapStateToProps, { emailChanged })(LoginForm);
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,12 +1,14 @@
 //one reducer responsibles for all thing authentication
-import { EMAIL_CHANGED } from './../components/actions/types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED } from './../components/actions/types';
 
-const INITIAL_STATE = { email: ''}
+const INITIAL_STATE = { email: '', password: ''}
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.payload}; //make a new object, return a new state if there's a new value
+    case PASSWORD_CHANGED:
+      return { ...state, password: action.payload};
     default:
       return state;
   }
