ec14b064d70b6d8630b9b3925a015e9ba6ef2fd0
Rex Low <qiweilow950823@gmail.com>
2016-10-19 20:28:05 +0800

    Installed lodash, instantiated component cycles for data pulling

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,12 +1,28 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { employeesFetch } from '../actions';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
+    this.createDataSource(this.props);
   }
+
+  componentWillReceiveProps(nextProps) {
+    // this.props is still the old set of props
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
   render() {
     return(
       <View>
@@ -19,4 +35,13 @@ class EmployeeList extends Component {
   }
 }
 
-export default connect(null, { employeesFetch })(EmployeeList);
+const mapStateToProps = state => {
+  //convert firebase object to array with lodash
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid};
+  });
+
+  return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
