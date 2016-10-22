6870afda8115018cedb7cb88fbc56413f20a0acf
Rex Low <qiweilow950823@gmail.com>
2016-10-22 19:59:59 +0800

    Added reducer method for data saving

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -10,7 +10,6 @@ import {
 const INITIAL_STATE = { email: '', password: '', user: null, error: '', loading: false};
 
 export default (state = INITIAL_STATE, action) => {
-  console.log(action);
   switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.payload}; //make a new object, return a new state if there's a new value
